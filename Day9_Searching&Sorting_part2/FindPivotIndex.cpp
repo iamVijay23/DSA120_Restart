451096f2ceb06884a045e378473bd5047a05231f
Trupti Ambade <vjmaurya232323@gmail.com>
2024-01-27 13:10:53 +0530

    Updated Comment & Day9_Completed

--- Day9_Searching&Sorting_part2/FindPivotIndex.cpp
+++ Day9_Searching&Sorting_part2/FindPivotIndex.cpp
@@ -1,6 +1,6 @@
 // Find the pivot Index of the array element
 //   Pivot -> it id maximum element in the array
-// solve it in the O(Log n)  using binary search
+// although it can be solved using the linear search and Sort function also  but solve it in the O(Log n)  using binary search
 
 #include <iostream>
 #include <vector>
@@ -22,13 +22,13 @@ int pivotIndex(vector<int> &arr)
             return start; // we can return mid or end also as there is single element
         }
 
-// mid + 1 < size && 
-         if ( mid + 1 < size &&  arr[mid] > arr[mid + 1]) // yeha condition mai galti kar sakte  h please pay more attention ... mid+1 check karna ki size ke bahar na jaaye
+        // mid + 1 < size &&
+        if (mid + 1 < size && arr[mid] > arr[mid + 1]) // yeha condition mai galti kar sakte  h please pay more attention ... mid+1 check karna ki size ke bahar na jaaye
         {
             return mid; // that will be our pivot index
         }
-        // mid - 1 >= 0 && 
-        else if ( mid - 1 >= 0 && arr[mid] < arr[mid - 1])
+        // mid - 1 >= 0 &&
+        else if (mid - 1 >= 0 && arr[mid] < arr[mid - 1])
         // yeha condition mai galti kar sakte  h please pay more attention ... mid-1 check karna ki  negative index na de
         {
             return mid - 1; // that will be our pivot index
@@ -51,9 +51,9 @@ int pivotIndex(vector<int> &arr)
 
 int main()
 {
-    vector<int> arr = { 12,14, 16 , 2, 4, 6, 8, 10}; 
-//  please dry run on the condition where {1,3 } and {2, 4,6,8,10}
-    int answer = pivotIndex(arr); 
+    vector<int> arr = {12, 14, 16, 2, 4, 6, 8, 10};
+    //  please dry run on the condition where {1,3 } and {2, 4,6,8,10}
+    int answer = pivotIndex(arr);
 
-    cout<<"Pivot index -> "<<answer<<endl;
+    cout << "Pivot index -> " << answer << endl;
 }
