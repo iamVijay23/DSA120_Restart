5ae57d9562f739eef2bed48cbf8d1a6aec6693b2
Trupti Ambade <vjmaurya232323@gmail.com>
2024-01-28 11:10:26 +0530

    Corrected The BruteForce Approach & Day11_Completed

--- Day11_QuestionSolving/FindPivotIndex.cpp
+++ Day11_QuestionSolving/FindPivotIndex.cpp
@@ -47,35 +47,44 @@ using namespace std;
 //     return -1;
 // }
 
-// BruteForce Approach for the finding the middle index -> Time Complexity O(N) and Space Complexity  O(N)
+// BruteForce Approach for the finding the Pivot index -> Time Complexity O(N) and Space Complexity  O(N)
+
 int findPivotIndex(vector<int> &nums)
 {
-    int sum = 0;
-    int leftSum = 0;
+    vector<int> sumLeft(nums.size(), 0);
+    vector<int> sumRight(nums.size(), 0);
 
-    // Calculate the total sum of the array
-    for (int i = 0; i < nums.size(); i++)
+    for (int i = 1; i < nums.size(); i++)
     {
-        sum += nums[i];
-    }
-
-    // Iterate through the array to find the pivot index
-    for (int i = 0; i < nums.size(); i++)
-    {
-        sum = sum - nums[i];
-
-        // Check if the current index is a pivot index
-        if (leftSum == sum)
+        int sum = 0;
+        for (int j = 0; j < i; j++)
         {
-            return i;
+            sum += nums[j];
         }
-
-        leftSum += nums[i];
+        sumLeft[i] = sum;
     }
 
-    // If no pivot index is found, return -1
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        int sum = 0;
+        for (int k = i + 1; k < nums.size(); k++)
+        {
+            sum += nums[k];
+        }
+        sumRight[i] = sum;
+    }
+
+    for (int j = 0; j < nums.size(); j++)
+    {
+        if (sumLeft[j] == sumRight[j])
+        {
+            return j;
+        }
+    }
+
     return -1;
 }
+
 int main()
 {
     vector<int> nums = {1, 7, 3, 6, 5, 6};
